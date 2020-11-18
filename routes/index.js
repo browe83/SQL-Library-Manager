6d4d264cecd7a16792c11d2254ecc2394c3e3da8
browe83 <rowe.brandon.j@gmail.com>
2020-11-18 11:53:47 -0500

    adds error handling, wires up css

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
 const { Book } = require('../models');
+var createError = require('http-errors');
 
 /* GET home page. */
-router.get('/', async function(req, res, next) {
+router.get('/', async (req, res, next) => {
   const books = await Book.findAll();
   console.log(books);
   res.json(books);
